5bf594327862b6e1e5c2b3f87d6b8781b8b01322
Simon Carr <simonjcarr@gmail.com>
2025-03-08 08:46:24 +0000

    ♻️ refactor(auth): consolidate auth components
    - remove Login component and replace with AuthButtons
    - add SignIn and SignOut functions to AuthButtons for better organization
    
    ✨ feat(site): introduce Header component
    
    - create and integrate Header component into layout
    - add SignIn and SignOut buttons to Header for authentication management

---
+++ src/app/components/auth/AuthButtons.tsx
@@ -0,0 +1,27 @@
+import { signIn, signOut } from "@/auth"
+
+export function SignIn() {
+  return (
+    <form
+      action={async () => {
+        "use server"
+        await signIn("dex")
+      }}
+    >
+      <button type="submit">Signin</button>
+    </form>
+  )
+} 
+
+export function SignOut() {
+  return (
+    <form
+      action={async () => {
+        "use server"
+        await signOut()
+      }}
+    >
+      <button type="submit">Sign Out</button>
+    </form>
+  )
+}
