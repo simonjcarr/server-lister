b90515631b992e059544e13391c4ed8bcc823bd8
Simon Carr <simonjcarr@gmail.com>
2025-03-09 10:26:13 +0000

    💄 style(location): remove unused imports
    - remove unnecessary imports of Card, Typography, and Tit from antd

--- src/app/location/add/page.tsx
+++ src/app/location/add/page.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import FormAddLocation from '@/app/components/location/FormAddLocation';
-import { Card, Typography, Tit } from 'antd';
-
-const { Title } = Typography;
 
 export default function AddLocationPage() {
   return (
