f642d507b4b4a3aa54b353d7e40ce64f1b05e34b
Simon Carr <simonjcarr@gmail.com>
2025-03-14 11:48:54 +0000

    ✨ feat(location): add getLocationById function
    - implement getLocationById to fetch location details by ID
    - handle errors gracefully and log them for debugging
    - throw a descriptive error when fetching location fails
    
    ✨ feat(server): enhance server details view with location
    
    - integrate location data into server details view
    - use react-query to manage location data fetching
    - gracefully handle loading and error states for location
    
    💄 style(view): improve layout in ViewServerDetails
    
    - update layout to use Row and Col for better structure
    - separate server and location details into distinct cards
    
    💄 style(page): adjust column span in server view page
    
    - change Col span from 8 to 24 for full-width display of server details

--- src/app/components/server/view/ViewServerDetails.tsx
+++ src/app/components/server/view/ViewServerDetails.tsx
@@ -1,6 +1,7 @@
+import { getLocationById } from '@/app/actions/location/crudActions'
 import { getServerById } from '@/app/actions/server/crudActions'
 import { useQuery } from '@tanstack/react-query'
-import { Alert, Card, Spin, Tag } from 'antd'
+import { Alert, Card, Col, Row, Spin, Tag } from 'antd'
 
 
 
@@ -9,19 +10,38 @@ function ViewServerDetails({ serverId }: { serverId: number }) {
     queryKey: ['server', serverId],
     queryFn: () => getServerById(serverId)
   })
+  const { data: location, isLoading: locationLoading, error: locationError } = useQuery({
+    queryKey: ['location', data?.locationId],
+    queryFn: () => getLocationById(data?.locationId || 0)
+  })
   return (
     <>
-    {isLoading && <Spin />}
-    {error && <Alert message="Error" description={error instanceof Error ? error.message : 'An error occurred'} type="error" />}
-    {data && (
-        <Card title={data.hostname} extra={ data.itar && <Tag color='#ff0000' >ITAR</Tag> }>
-        <div className='my-4 text-gray-400'>{data.description}</div>
-        <p>Hostname: {data.hostname}</p>
-        <p>IPV4: {data.ipv4}</p>
-        <p>IPV6: {data.ipv6}</p>
-        <p>Build Doc: <a href={data.docLink} target="_blank" rel="noopener noreferrer">{data.docLink}</a></p>
-      </Card>
-    )}
+      {isLoading && <Spin />}
+      {error && <Alert message="Error" description={error instanceof Error ? error.message : 'An error occurred'} type="error" />}
+      {data && (
+        <Card title={`Hostname: ${data.hostname}`} extra={data.itar && <Tag color='#ff0000' >ITAR</Tag>}>
+          <Row gutter={[16, 16]}>
+            <Col>
+              <Card title="Server Details">
+                <div className='my-4 text-gray-400'>{data.description}</div>
+                <p>Hostname: {data.hostname}</p>
+                <p>IPV4: {data.ipv4}</p>
+                <p>IPV6: {data.ipv6}</p>
+                {data.docLink && <p>Build Doc: <a href={data.docLink} target="_blank" rel="noopener noreferrer">{data.docLink}</a></p> || <p>No Build Doc</p>}
+              </Card>
+            </Col>
+            <Col>
+              {locationLoading && <Spin />}
+              {locationError && <Alert message="Error" description={locationError instanceof Error ? locationError.message : 'An error occurred'} type="error" />}  
+              {location && (
+                <Card title={`Location: ${location.name}`}>
+                  <p>{location.name}</p>
+                </Card>
+              )}
+            </Col>
+          </Row>
+        </Card>
+      )}
     </>
   )
 }
