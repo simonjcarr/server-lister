0a113862391713661b18b065888776a5b58d3122
Simon Carr <simonjcarr@gmail.com>
2025-03-14 11:37:57 +0000

    ♻️ refactor(crudActions): rename getServer function
    - change function name from getServer to getServerById
    - improve clarity by indicating the function retrieves by ID
    
    ✨ feat(view): enhance server details view
    
    - add react-query for data fetching
    - implement loading spinner with Spin component
    - display error messages with Alert component
    - render server details in Card format with optional ITAR tag

--- src/app/components/server/view/ViewServerDetails.tsx
+++ src/app/components/server/view/ViewServerDetails.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { getServerById } from '@/app/actions/server/crudActions'
+import { useQuery } from '@tanstack/react-query'
+import { Alert, Card, Spin, Tag } from 'antd'
+
+
 
 function ViewServerDetails({ serverId }: { serverId: number }) {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['server', serverId],
+    queryFn: () => getServerById(serverId)
+  })
   return (
-    <div>ViewServerDetails {serverId}</div>
+    <>
+    {isLoading && <Spin />}
+    {error && <Alert message="Error" description={error instanceof Error ? error.message : 'An error occurred'} type="error" />}
+    {data && (
+        <Card title={data.hostname} extra={ data.itar && <Tag color='#ff0000' >ITAR</Tag> }>
+        <div className='my-4 text-gray-400'>{data.description}</div>
+        <p>Hostname: {data.hostname}</p>
+        <p>IPV4: {data.ipv4}</p>
+        <p>IPV6: {data.ipv6}</p>
+        <p>Build Doc: <a href={data.docLink} target="_blank" rel="noopener noreferrer">{data.docLink}</a></p>
+      </Card>
+    )}
+    </>
   )
 }
 
