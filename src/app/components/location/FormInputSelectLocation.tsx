f5368487089c8c74d03ef293f4f0deb1798ab4d7
Simon Carr <simonjcarr@gmail.com>
2025-03-17 09:47:59 +0000

    ``` ✨ feat(location): add FormInputSelectLocation component
    - introduce a new FormInputSelectLocation component for selecting locations
    - utilize react-query for fetching location data
    
    ✨ feat(server): add FormEditServer component
    
    - create a new FormEditServer component for editing server information
    - integrate FormInputSelectLocation for location selection
    
    ✨ feat(server): implement server edit page
    
    - add a new edit server page with form capabilities
    - integrate FormEditServer for editing server details
    
    🐛 fix(server): enable navigation to edit server page
    
    - update button action to navigate to edit page on click
    ```

---
+++ src/app/components/location/FormInputSelectLocation.tsx
@@ -0,0 +1,30 @@
+'use client'
+import { Alert, Select, Spin } from 'antd'
+import { getLocations } from '@/app/actions/location/crudActions'
+import { useQuery } from '@tanstack/react-query'
+
+interface FormInputSelectLocationProps {
+  value?: number;
+  onChange?: (value: number) => void;
+}
+
+const FormInputSelectLocation = ({ value, onChange }: FormInputSelectLocationProps) => {
+  const { data: locations, isLoading, error } = useQuery({
+    queryKey: ["locations"],
+    queryFn: () => getLocations(),
+  })
+  return (
+    <Select
+      value={value}
+      onChange={onChange}
+      loading={isLoading}
+      placeholder="Select a location"
+      options={locations?.map((location) => ({
+        value: location.id,
+        label: location.name,
+      }))}
+    />
+  )
+}
+
+export default FormInputSelectLocation
