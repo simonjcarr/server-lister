c0bf5def2d6bbe2fa585c38f32024555221fbc45
Simon Carr <simonjcarr@gmail.com>
2025-03-17 11:59:39 +0000

    ✨ feat(server): add click-to-copy functionality for server details
    - introduce ClickToCopy component to enable easy copying of server details
    - apply ClickToCopy to hostname, IPv4, and IPv6 in ServerList and ViewServerDetails
    - enhance user experience with visual feedback for copy actions

--- src/app/components/server/view/ViewServerDetails.tsx
+++ src/app/components/server/view/ViewServerDetails.tsx
@@ -2,6 +2,8 @@ import { getLocationById } from '@/app/actions/location/crudActions'
 import { getServerById } from '@/app/actions/server/crudActions'
 import { useQuery } from '@tanstack/react-query'
 import { Alert, Card, Col, Row, Spin, Tag } from 'antd'
+import { CopyOutlined } from '@ant-design/icons';
+import ClickToCopy from '../../utils/ClickToCopy';
 
 function ViewServerDetails({ serverId }: { serverId: number }) {
   const { data: serverData, isLoading, error } = useQuery({
@@ -13,6 +15,7 @@ function ViewServerDetails({ serverId }: { serverId: number }) {
     queryFn: () => serverData?.locationId ? getLocationById(serverData!.locationId) : null,
     enabled: !!serverData?.locationId,
   });
+
   return (
     <>
       {isLoading && <Spin />}
@@ -20,9 +23,18 @@ function ViewServerDetails({ serverId }: { serverId: number }) {
       {serverData && (
         <Card title="Server Details" extra={serverData.itar && <Tag color='#ff0000' >ITAR</Tag>}>
           <div className='my-4 text-gray-400'>{serverData.description}</div>
-          <p>Hostname: {serverData.hostname}</p>
-          <p>IPV4: {serverData.ipv4}</p>
-          <p>IPV6: {serverData.ipv6}</p>
+          <p>
+            Hostname: 
+            <ClickToCopy text={serverData.hostname ?? ''} />
+          </p>
+          <p>
+            IPV4: 
+            <ClickToCopy text={serverData.ipv4 ?? ''} />
+          </p>
+          <p>
+            IPV6: 
+            <ClickToCopy text={serverData.ipv6 ?? ''} />
+          </p>
           {serverData.docLink && <p>Build Doc: <a href={serverData.docLink} target="_blank" rel="noopener noreferrer">{serverData.docLink}</a></p> || <p>No Build Doc</p>}
         </Card>
       )
