2b6d6670fac497d9978c30790bb166f5ad6770f7
Simon Carr <simonjcarr@gmail.com>
2025-03-20 09:59:14 +0000

    ✨ feat(notes): add modal display for notes
    - create DisplayNoteInModal component to show note details in a modal
    - integrate modal into ViewServerNotes for enhanced note viewing experience
    
    ✨ feat(notes): limit note preview length
    
    - modify ViewServerNotes to show only the first 40 characters of each note
    
    ✨ feat(utils): add popover to DistanceToNow
    
    - enhance DistanceToNow with a popover displaying the full date and time
    
    📦 build(dependencies): add dompurify for sanitizing HTML content
    
    - include dompurify package for secure HTML rendering within the notes modal

---
+++ src/app/components/server/view/notes/DisplayNoteInModal.tsx
@@ -0,0 +1,57 @@
+'use client'
+import { Button, Modal} from "antd"
+import { FullscreenOutlined } from "@ant-design/icons"
+import { useQuery } from "@tanstack/react-query"
+import { getNoteById } from "@/app/actions/server/notes/crudServerNoteActions"
+import DistanceToNow from "@/app/components/utils/DistanceToNow"
+import { useState } from "react"
+import DOMPurify from "dompurify"
+
+const DisplayNoteInModal = ({ noteId }: { noteId: number }) => {
+  const { data: note, isLoading, error } = useQuery({
+    queryKey: ["note", noteId],
+    queryFn: () => getNoteById(noteId),
+    enabled: !!noteId
+  })
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  
+  return (
+    <>
+      <Button size="small" type="primary" onClick={showModal}>
+        <FullscreenOutlined />
+      </Button>
+      <Modal title={note && (
+        <div className="flex justify-between font-semibold">
+          <div>{note?.userName}</div>
+          <div><DistanceToNow date={note?.createdAt} /></div>
+        </div>
+      )} open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+      <>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+      {note && (
+        <>
+          {/* Only the first 40 characters of the note should be displayed */}
+          <div className="whitespace-pre-line max-h-[300px] overflow-auto" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(note?.note.replace(/\n/g, "<br />")) }}></div>
+        </>
+      )}
+      </>
+    </Modal>
+    </>
+  )
+}
+
+export default DisplayNoteInModal
