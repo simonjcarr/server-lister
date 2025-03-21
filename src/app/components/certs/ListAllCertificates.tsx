d5e3e5acedb5088b0c2ff1f85fa9065221600163
Simon Carr <simonjcarr@gmail.com>
2025-03-21 23:33:14 +0000

    ✨ feat(certs): add getAllCertificates function
    - introduce a new function to fetch all certificates including server and user data
    - enhance data retrieval with left joins for comprehensive information
    
    ✨ feat(certs): create certificate management page
    
    - add a new page component for managing certificates
    - integrate ListAllCertificates component for displaying certificate data
    
    ✨ feat(certs): add CertStatus component
    
    - implement a new component to display certificate status with color coding
    
    ✨ feat(certs): implement ListAllCertificates component
    
    - create a component to list all certificates with filtering and sorting capabilities
    - integrate CertStatus and ClickToCopy components for enhanced functionality
    
    ♻️ refactor(server): reuse CertStatus component
    
    - replace duplicated CertStatus implementation with shared component usage
    
    ✨ feat(nav): integrate certificates into navigation
    
    - add certificates section in navigation drawer with links to manage certificates page

---
+++ src/app/components/certs/ListAllCertificates.tsx
@@ -0,0 +1,85 @@
+'use client'
+import React from 'react'
+import { useQuery } from "@tanstack/react-query"
+import { getAllCertificates } from '@/app/actions/certs/crudActions'
+import { Input, Table, Tag } from 'antd'
+import CertStatus from '@/app/components/certs/CertStatus'
+import ClickToCopy from '../utils/ClickToCopy'
+import { SearchOutlined } from '@ant-design/icons'
+
+const ListAllCertificates = () => {
+  const [searchText, setSearchText] = React.useState('')
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['certs'],
+    queryFn: () => getAllCertificates()
+  })
+  return (
+    <div>
+      <div className="text-2xl font-bold mb-4">All Certificates</div>
+      <Input
+        className='mb-4'
+        value={searchText}
+        size='large'
+        onChange={(e) => setSearchText(e.target.value)}
+        placeholder="Search"
+        prefix={<SearchOutlined />}
+      />
+      <div>
+        Total Records: {data?.length || 0} | Filtered Records: {data?.filter(cert => cert.primaryDomain.includes(searchText) || (cert.otherDomains as {domain: string}[]).some(domain => domain.domain.includes(searchText)) || cert.status.includes(searchText) || cert.server?.name.includes(searchText) || (cert.requestedBy?.name || '').includes(searchText)).length || 0}
+      </div>
+    {isLoading && <p>Loading...</p>}
+    {error && <p>Error: {error.message}</p>}
+    {data && (
+      <Table
+        size="small"
+        columns={[
+          { 
+            title: 'Status', 
+            dataIndex: 'status', 
+            render: (text) => <CertStatus cert={text} />, sorter: (a, b) => (a.status && b.status) ? a.status.localeCompare(b.status) : 0, sortDirections: ['ascend', 'descend'], defaultSortOrder: 'ascend' ,
+            filters: [
+              { text: 'Pending', value: 'Pending' },
+              { text: 'Ordered', value: 'Ordered' },
+              { text: 'Ready', value: 'Ready' },
+            ],
+            onFilter: (value, record) => record.status === value
+          },
+          { 
+            title: 'Primary Domain', 
+            dataIndex: 'primaryDomain', 
+            render: (text) => <ClickToCopy text={text} />, 
+            sorter: (a, b) => (a.primaryDomain && b.primaryDomain) ? a.primaryDomain.localeCompare(b.primaryDomain) : 0, sortDirections: ['ascend', 'descend'], defaultSortOrder: 'ascend',
+            filters: data.map(cert => ({text: cert.primaryDomain, value: cert.primaryDomain})),
+            onFilter: (value, record) => record.primaryDomain === value
+          },
+          { 
+            title: 'Other Domains', 
+            dataIndex: 'otherDomains',
+            render: (text) => {
+              return text.map((domain: {domain: string}) => <Tag key={domain.domain}>{domain.domain}</Tag>)
+            },
+            
+          },
+          { 
+            title: 'Server Host', 
+            dataIndex: ['server', 'name'],
+            render: (text) => text || 'N/A',
+            sorter: (a, b) => (a.server?.name && b.server?.name) ? a.server.name.localeCompare(b.server.name) : 0, sortDirections: ['ascend', 'descend'], defaultSortOrder: 'ascend'
+          },
+          { 
+            title: 'Requested By', 
+            dataIndex: ['requestedBy', 'name'],
+            render: (text) => text || 'N/A',
+            sorter: (a, b) => (a.requestedBy?.name && b.requestedBy?.name) ? a.requestedBy.name.localeCompare(b.requestedBy.name) : 0, sortDirections: ['ascend', 'descend'], defaultSortOrder: 'ascend'
+          },
+        ]}
+        dataSource={data.filter(cert => cert.primaryDomain.includes(searchText) || (cert.otherDomains as {domain: string}[]).some(domain => domain.domain.includes(searchText)) || cert.status.includes(searchText) || cert.server?.name.includes(searchText) || (cert.requestedBy?.name || '').includes(searchText)).map(cert => ({ ...cert, key: cert.id }))}
+        loading={isLoading}
+        size="small"
+      />
+    )}
+    </div>
+  )
+}
+
+export default ListAllCertificates
