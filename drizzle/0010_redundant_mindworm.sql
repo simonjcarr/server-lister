eb0a2b2399422d558eb0b8ec852716d80a6d5c86
Simon Carr <simonjcarr@gmail.com>
2025-03-22 19:05:59 +0000

    🔧 chore(env): add API_URL placeholder to .env.example
    - add API_URL to .env.example for API endpoint configuration
    
    🐛 fix(database): rename column description to message in notifications
    
    - change column name in notifications table for clarity
    
    ✨ feat(api): add notification route for sending notifications
    
    - implement POST route to send notifications based on roles or user IDs
    - validate input and handle errors during notification creation
    
    🐛 fix(schema): update notification schema for message field
    
    - update notifications schema to reflect column name change
    
    ✨ feat(worker): integrate postNotification in worker
    
    - add postNotification function to send notifications via API
    - ensure API_URL is defined in environment variables

---
+++ drizzle/0010_redundant_mindworm.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "notifications" RENAME COLUMN "description" TO "message";
---
+++ src/app/api/workers/notification/route.ts
@@ -0,0 +1,50 @@
+import { db } from "@/db";
+import { eq, or, sql } from "drizzle-orm";
+import { notifications, users } from "@/db/schema";
+export async function POST(request: Request) {
+  
+  const { title, message, roleNames, userIds}: {title: string, message: string, roleNames?: string[], userIds?: []} = await request.json();
+  // Roles is a json field with array of roles. Write a db query that select users that have the roles in the request array
+  if((!roleNames || roleNames.length === 0) && (!userIds || userIds.length === 0)) {
+    return new Response('You must provide at least one role or user as an array', { status: 400 })
+  }
+
+  const roleConditions = roleNames?.map(role => 
+    sql`${users.roles} ? ${role}`
+  ) || [];
+
+  const userConditions = userIds?.map(user => 
+    eq(users.id, user)
+  ) || [];
+
+
+  const allConditions = [...roleConditions, ...userConditions];
+
+  const usersResult = await db
+    .select()
+    .from(users)
+    .where(or(...allConditions))
+    .groupBy(users.id);
+
+  const errors: string[] = [];
+  for (const user of usersResult) {
+    try {
+      await db.insert(notifications).values({
+        userId: user.id,
+        message,
+        title,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        read: false
+      })
+    } catch {
+      console.error(`error creating notification for user ${user.id}`);
+      errors.push(`error creating notification for user ${user.id}`);
+    }
+  }
+
+  if (errors.length > 0) {
+    return new Response(JSON.stringify({ errors }), { status: 500 });
+  }
+  return new Response('Notifications sent successfully', { status: 200 });
+}
