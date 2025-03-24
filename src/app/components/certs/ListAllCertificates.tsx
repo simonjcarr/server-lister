d97fc02d4f2002ece32b4e693b828bd322c5461c
Simon Carr <simonjcarr@gmail.com>
2025-03-24 08:11:00 +0000

    🐛 fix(certs): enhance search functionality for certificates
    - ensure search is case-insensitive for improved usability
    - refactor filtering logic to accommodate edge cases with otherDomains

--- src/app/components/certs/ListAllCertificates.tsx
+++ src/app/components/certs/ListAllCertificates.tsx
@@ -26,61 +26,69 @@ const ListAllCertificates = () => {
         prefix={<SearchOutlined />}
       />
       <div>
-        Total Records: {data?.length || 0} | Filtered Records: {data?.filter(cert => cert.primaryDomain.includes(searchText) || (cert.otherDomains as {domain: string}[]).some(domain => domain.domain.includes(searchText)) || cert.status.includes(searchText) || cert.server?.name.includes(searchText) || (cert.requestedBy?.name || '').includes(searchText)).length || 0}
+        Total Records: {data?.length || 0} | Filtered Records: {data?.filter(cert => cert.primaryDomain.includes(searchText) || (Array.isArray(cert.otherDomains) ? cert.otherDomains : []).some(domain => domain.domain.includes(searchText)) || cert.status.includes(searchText) || cert.server?.name.includes(searchText) || (cert.requestedBy?.name || '').includes(searchText)).length || 0}
       </div>
-    {isLoading && <p>Loading...</p>}
-    {error && <p>Error: {error.message}</p>}
-    {data && (
-      <Table
-        size="small"
-        columns={[
-          { 
-            title: 'Status', 
-            dataIndex: 'status', 
-            render: (text) => <CertStatus status={text} />, sorter: (a, b) => (a.status && b.status) ? a.status.localeCompare(b.status) : 0, sortDirections: ['ascend', 'descend'], defaultSortOrder: 'ascend' ,
-            filters: [
-              { text: 'Pending', value: 'Pending' },
-              { text: 'Ordered', value: 'Ordered' },
-              { text: 'Ready', value: 'Ready' },
-            ],
-            onFilter: (value, record) => record.status === value
-          },
-          { 
-            title: 'Primary Domain', 
-            dataIndex: 'primaryDomain', 
-            render: (text) => <ClickToCopy text={text} />, 
-            sorter: (a, b) => (a.primaryDomain && b.primaryDomain) ? a.primaryDomain.localeCompare(b.primaryDomain) : 0, sortDirections: ['ascend', 'descend'], defaultSortOrder: 'ascend',
-            filters: data.map(cert => ({text: cert.primaryDomain, value: cert.primaryDomain})),
-            onFilter: (value, record) => record.primaryDomain === value
-          },
-          { 
-            title: 'Other Domains', 
-            dataIndex: 'otherDomains',
-            render: (text) => {
-              return text && text.map((domain: {domain: string}) => <Tag key={domain.domain}>{domain.domain}</Tag>)
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+      {data && (
+        <Table
+          size="small"
+          columns={[
+            {
+              title: 'Status',
+              dataIndex: 'status',
+              render: (text) => <CertStatus status={text} />, sorter: (a, b) => (a.status && b.status) ? a.status.localeCompare(b.status) : 0, sortDirections: ['ascend', 'descend'], defaultSortOrder: 'ascend',
+              filters: [
+                { text: 'Pending', value: 'Pending' },
+                { text: 'Ordered', value: 'Ordered' },
+                { text: 'Ready', value: 'Ready' },
+              ],
+              onFilter: (value, record) => record.status === value
             },
-            
-          },
-          { 
-            title: 'Server Host', 
-            dataIndex: ['server', 'name'],
-            render: (text) => text || 'N/A',
-            sorter: (a, b) => (a.server?.name && b.server?.name) ? a.server.name.localeCompare(b.server.name) : 0, sortDirections: ['ascend', 'descend'], defaultSortOrder: 'ascend'
-          },
-          { 
-            title: 'Requested By', 
-            dataIndex: ['requestedBy', 'name'],
-            render: (text) => text || 'N/A',
-            sorter: (a, b) => (a.requestedBy?.name && b.requestedBy?.name) ? a.requestedBy.name.localeCompare(b.requestedBy.name) : 0, sortDirections: ['ascend', 'descend'], defaultSortOrder: 'ascend'
-          },
-          {
-            render: (text, record) => <CertDropDownMenu certId={record.id}><MoreOutlined /></CertDropDownMenu>
-          }
-        ]}
-        dataSource={data.filter(cert => cert.primaryDomain.includes(searchText) || (cert.otherDomains as {domain: string}[]).some(domain => domain.domain.includes(searchText)) || cert.status.includes(searchText) || cert.server?.name.includes(searchText) || (cert.requestedBy?.name || '').includes(searchText)).map(cert => ({ ...cert, key: cert.id }))}
-        loading={isLoading}
-      />
-    )}
+            {
+              title: 'Primary Domain',
+              dataIndex: 'primaryDomain',
+              render: (text) => <ClickToCopy text={text} />,
+              sorter: (a, b) => (a.primaryDomain && b.primaryDomain) ? a.primaryDomain.localeCompare(b.primaryDomain) : 0, sortDirections: ['ascend', 'descend'], defaultSortOrder: 'ascend',
+              filters: data.map(cert => ({ text: cert.primaryDomain, value: cert.primaryDomain })),
+              onFilter: (value, record) => record.primaryDomain === value
+            },
+            {
+              title: 'Other Domains',
+              dataIndex: 'otherDomains',
+              render: (text) => {
+                return text && text.map((domain: { domain: string }) => <Tag key={domain.domain}>{domain.domain}</Tag>)
+              },
+
+            },
+            {
+              title: 'Server Host',
+              dataIndex: ['server', 'name'],
+              render: (text) => text || 'N/A',
+              sorter: (a, b) => (a.server?.name && b.server?.name) ? a.server.name.localeCompare(b.server.name) : 0, sortDirections: ['ascend', 'descend'], defaultSortOrder: 'ascend'
+            },
+            {
+              title: 'Requested By',
+              dataIndex: ['requestedBy', 'name'],
+              render: (text) => text || 'N/A',
+              sorter: (a, b) => (a.requestedBy?.name && b.requestedBy?.name) ? a.requestedBy.name.localeCompare(b.requestedBy.name) : 0, sortDirections: ['ascend', 'descend'], defaultSortOrder: 'ascend'
+            },
+            {
+              render: (text, record) => <CertDropDownMenu certId={record.id}><MoreOutlined /></CertDropDownMenu>
+            }
+          ]}
+          dataSource={
+            data.filter(
+              cert => cert.primaryDomain.toLowerCase().includes(searchText.toLowerCase()) ||
+                (Array.isArray(cert.otherDomains) ? cert.otherDomains : []).some((domain: { domain: string }) => domain.domain.toLowerCase().includes(searchText.toLowerCase())) ||
+              cert.status.toLowerCase().includes(searchText.toLowerCase()) ||
+              cert.server?.name.toLowerCase().includes(searchText.toLowerCase()) ||
+              (cert.requestedBy?.name?.toLowerCase() || '').includes(searchText.toLowerCase())
+            )
+            .map(cert => ({ ...cert, key: cert.id }))}
+          loading={isLoading}
+        />
+      )}
     </div>
   )
 }
