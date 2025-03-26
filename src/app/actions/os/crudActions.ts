68adfdc882a3a633d38760e982ea6c83bd6a89c9
Simon Carr <simonjcarr@gmail.com>
2025-03-26 11:35:44 +0000

    ✨ feat(os): add getOSs function to retrieve list of operating systems
    - introduce getOSs function to fetch OS list with latest patch version
    - handle errors and log appropriate messages
    
    💄 style(crudActions): standardize string quotes in error messages
    
    - change single quotes to double quotes for consistency
    
    ✨ feat(os): enhance FormAddOS with drawer functionality
    
    - add drawer to FormAddOS for better user interaction
    - include toggle functionality for opening and closing the drawer
    
    ✨ feat(site): integrate FormAddOS into navDrawer
    
    - add FormAddOS component to the navigation drawer
    - remove redundant onClick routing for Add OS item
    
    ✨ feat(os): create page component for displaying OS list
    
    - implement OS list page with table to display OS data
    - utilize react-query for data fetching
    - handle loading and error states in UI

--- src/app/actions/os/crudActions.ts
+++ src/app/actions/os/crudActions.ts
@@ -1,9 +1,9 @@
-'use server';
+"use server";
 
 import { db } from "@/db";
-import { os } from "@/db/schema";
-import { eq } from "drizzle-orm";
-import type { SelectOS, InsertOS, UpdateOS } from "@/db/schema";
+import { os, osPatchVersions } from "@/db/schema";
+import { eq, sql } from "drizzle-orm";
+import type { InsertOS, UpdateOS } from "@/db/schema";
 
 export async function getOS() {
   try {
@@ -60,11 +60,39 @@ export async function getOSById(id: number) {
   try {
     const osData = await db.select().from(os).where(eq(os.id, id)).limit(1);
     if (osData.length === 0) {
-      throw new Error('OS not found');
-    } 
+      throw new Error("OS not found");
+    }
     return osData[0];
   } catch (error) {
     console.error("Error getting OS by ID:", error);
     throw new Error("Failed to get OS by ID");
+  }
+}
+
+export async function getOSs() {
+  try {
+    const osResult = await db
+      .select({
+        id: os.id,
+        name: os.name,
+        EOLDate: os.EOLDate,
+        createdAt: os.createdAt,
+        updatedAt: os.updatedAt,
+        latestPatchVersion: sql`COALESCE(
+        (
+          SELECT ${osPatchVersions.patchVersion}
+          FROM ${osPatchVersions}
+          WHERE ${osPatchVersions.osId} = ${os.id}
+          ORDER BY ${osPatchVersions.patchVersion} DESC
+          LIMIT 1
+        ),
+        'No patch version'
+      )`,
+      })
+      .from(os);
+    return osResult;
+  } catch (error) {
+    console.error("Error getting OSs:", error);
+    throw new Error("Failed to get OSs");
   }
 }
