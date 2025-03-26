c48db493667822648c2943234792f15a26d8c67f
Simon Carr <simonjcarr@gmail.com>
2025-03-26 08:49:43 +0000

    ✨ feat(hardwareTab): enhance hardware data display
    - add Statistic component for improved visualization of CPU cores and RAM
    - remove plain text display for CPU cores and RAM
    - maintain error handling with Alert component

--- src/app/components/server/view/HardwareTabData.tsx
+++ src/app/components/server/view/HardwareTabData.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query"
 import { getServerById } from "@/app/actions/server/crudActions"
-import { Alert, Spin } from "antd"
+import { Alert, Spin, Statistic } from "antd"
 const HardwareTabData = ({ serverId }: { serverId: number }) => {
   const { data, error, isLoading } = useQuery({
     queryKey: ["server", serverId],
@@ -13,9 +13,10 @@ const HardwareTabData = ({ serverId }: { serverId: number }) => {
       {error && <Alert message="Error" description={error instanceof Error ? error.message : 'An error occurred'} type="error" />}
       {data && (
         <>
-          <p>Cores: {data.cores}</p>
-          <p>RAM: {data.ram}</p>
-          <p>Storage: {data.diskSpace}</p>
+        <div className="flex gap-8">
+          { data.cores && <Statistic title="CPU Cores" value={data.cores} />}
+          { data.ram && <Statistic title="RAM" value={data.ram} />}
+        </div>
         </>
       )}
     </div>
