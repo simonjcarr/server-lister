67b492fa2de851a789d8028b98532e0de6060cb4
Simon Carr <simonjcarr@gmail.com>
2025-03-29 08:53:01 +0000

    remove unused files

--- src/app/api/servers/favorites/details/route.ts
+++
@@ -1,5 +0,0 @@
-'use client';
-
-// This file is kept as a placeholder.
-// All server favorites details functionality has been moved to server actions in:
-// /app/actions/server/userServerActions.ts
--- src/app/api/servers/route.ts
+++
@@ -1,5 +0,0 @@
-'use client';
-
-// This file is kept as a placeholder.
-// All server functionality has been moved to server actions in:
-// /app/actions/server/userServerActions.ts
