391d6d1335df8cffb352a77062626dc26d2198b0
Simon Carr <simonjcarr@gmail.com>
2025-04-04 16:39:54 +0100

    🔧 chore(env): update database configuration for testing
    - update DATABASE_URL with new credentials and host details
    - add separate environment variables for host, port, user, and password
    
    ✨ feat(config): add test configuration for database migrations
    
    - create drizzle.config.test.ts for test environment
    - configure database credentials and migration paths for testing

---
+++ drizzle.config.test.ts
@@ -0,0 +1,22 @@
+// The migrate command to run for test is as follows
+// NODE_ENV=test TEST_DATABASE_NAME=ims_test npx -y dotenv -e .env.test -- drizzle-kit migrate --config=drizzle.config.test.ts
+
+import type { Config } from "drizzle-kit";
+// import * as dotenv from 'dotenv';
+// dotenv.config({ path: '.env.test' });
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not defined in .env.test');
+}
+
+const databaseName = process.env.TEST_DATABASE_NAME
+const databaseURL = `postgres://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${databaseName}`
+
+export default {
+  schema: "./src/db/schema/index.ts", // Path to schema definitions
+  out: "./drizzle", // Output folder for migrations
+  dialect: "postgresql",
+  dbCredentials: {
+    url: databaseURL,
+  },
+} satisfies Config;
