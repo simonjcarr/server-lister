b2c2b397358750be434fe80eebc098eacc272e52
Simon Carr <simonjcarr@gmail.com>
2025-04-14 12:46:44 +0100

    ♻️ refactor(db): update database import path
    - change db import to use default export from getdb for consistency

--- src/app/actions/projects/crudActions.ts
+++ src/app/actions/projects/crudActions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { db } from '@/db';
+import db from '@/db/getdb';
 import { projects, business, primaryProjectEngineers, users, drawings, projectDrawings } from '@/db/schema';
 import { revalidatePath } from 'next/cache';
 import { eq } from 'drizzle-orm';
