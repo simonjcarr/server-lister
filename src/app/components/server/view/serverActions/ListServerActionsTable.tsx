af042c06e8e842c1e6a92d4e948c5f089d375854
Simon Carr <simonjcarr@gmail.com>
2025-04-23 09:17:30 +0100

    ✨ feat(server): enhance server actions table and add view component
    - add new fields to ServerAction type for enhanced data handling
    - implement row selection in ListServerActionsTable with useState
    - display selected row's title for user feedback
    - create ViewServerAction component for server action details display

--- src/app/components/server/view/serverActions/ListServerActionsTable.tsx
+++ src/app/components/server/view/serverActions/ListServerActionsTable.tsx
@@ -3,12 +3,19 @@ import { getServerActions } from "@/app/actions/serverActions/crudActions"
 import { useQuery } from "@tanstack/react-query"
 import { Table } from "antd"
 import CreateNewServerActionForm from "./CreateNewServerActionForm"
+import { useState } from "react"
 
 // Define the type for a server action row
 export type ServerAction = {
   id: number;
   title: string;
   assignedTo: string;
+  userId: string;
+  userName: string | null;
+  userEmail: string | null;
+  createdAt: Date;
+  updatedAt: Date;
+  key: number;
   // Add other fields as needed
 };
 
@@ -27,6 +34,7 @@ const columns = [
 ]
 
 const ListServerActionsTable = ({ serverId }: { serverId: number }) => {
+  const [selectedRow, setSelectedRow] = useState<ServerAction | null>(null)
   const { data, error, isLoading } = useQuery({
     queryKey: ["serverActions", serverId],
     queryFn: () => getServerActions(serverId).then(data => data.map(item => ({ key: item.id, ...item, assignedTo: item.userId }))),
@@ -49,10 +57,14 @@ const ListServerActionsTable = ({ serverId }: { serverId: number }) => {
           pagination={false}
           rowHoverable={true}
           rowClassName={"cursor-pointer"}
+          onRow={(record) => ({
+            onClick: () => setSelectedRow(record),
+          })}
         />
       ) : (
         <p>No actions found for this server</p>
       )}
+      {selectedRow && selectedRow.title}
     </div>
   )
 }
---
+++ src/app/components/server/view/serverActions/ViewServerAction.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ViewServerAction = () => {
+  return (
+    <div>ViewServerAction</div>
+  )
+}
+
+export default ViewServerAction
