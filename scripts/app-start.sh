d1c55d77ea92fb58c7966efc30c8b148c3439010
Simon Carr <simonjcarr@gmail.com>
2025-05-19 16:00:04 +0100

    🔥 chore(cleanup): remove unused docker and sql files
    - deleted obsolete docker-compose and sql files
    - removed corresponding meta data files for cleanup
    
    💄 style(app-start): comment out PostgreSQL readiness check
    
    - commented out the PostgreSQL readiness wait in app-start script
    - retained migration command for database setup

--- scripts/app-start.sh
+++ scripts/app-start.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Wait for PostgreSQL to be ready
-echo "Waiting for PostgreSQL to be ready..."
-until pg_isready -h postgres -U ${POSTGRES_USER:-postgres} -d ${POSTGRES_DB:-ims} -t 1; do
-  echo "Waiting for PostgreSQL..."
-  sleep 2
-done
+# echo "Waiting for PostgreSQL to be ready..."
+# until pg_isready -h postgres -U ${POSTGRES_USER:-postgres} -d ${POSTGRES_DB:-ims} -t 1; do
+#   echo "Waiting for PostgreSQL..."
+#   sleep 2
+# done
 
 # Migrate the database
 echo "Running database migrations..."
